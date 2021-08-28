650e6346d1bb01e48271c6b23b99d927ee556253
Nurul Yanti Cahaya <nycovering@gmail.com>
2021-08-28 17:58:00 +0700

    Update

---
+++ js/products.js
@@ -0,0 +1,104 @@
+import img1 from '../images/det1.jpeg';
+import img2 from '../images/det4.jpeg';
+import img3 from '../images/det7.jpg';
+import img4 from '../images/det10.jpg';
+import img5 from '../images/det13.jpg';
+import img6 from '../images/det16.jpg';
+import img7 from '../images/det21.jpg';
+import img8 from '../images/det22.jpg';
+import img9 from '../images/det25.jpg';
+import img10 from '../images/det33.jpg';
+
+
+const products = [
+    {
+        img: img1,
+        id: 1,
+        alt: 'Farhah Khimar Baby Blue',
+        name: 'Farhah Khimar Baby Blue',
+        desc: 'Farhah Khimar Baby Blue',
+        price: 100000,
+        size: 'M'
+    },
+    {
+        img: img2,
+        id: 2,
+        alt: 'Farhah Khimar Candy Pink',
+        name: 'Farhah Khimar Candy Pink',
+        desc: 'Farhah Khimar Candy Pink',
+        price: 100000,
+        size: 'M'
+    },
+    {
+        img: img3,
+        id: 3,
+        alt: 'Misha Khimar Coffee',
+        name: 'Misha Khimar Coffee',
+        desc: 'Misha Khimar Coffee',
+        price: 80000,
+        size: 'M'
+    },
+    {
+        img: img4,
+        id: 4,
+        alt: 'Misha Khimar Nude',
+        name: 'Misha Khimar Nude',
+        desc: 'Misha Khimar Nude',
+        price: 80000,
+        size: 'M'
+    },
+    {
+        img: img5,
+        id: 5,
+        alt: 'Farhah Khimar Rugby Tan',
+        name: 'FFarhah Khimar Rugby Tan',
+        desc: 'Farhah Khimar Rugby Tan',
+        price: 150000,
+        size: 'L'
+    },
+    {
+        img: img6,
+        id: 6,
+        alt: 'Farhah Khimar Peach Beige',
+        name: 'Farhah Khimar Peach Beige',
+        desc: 'Farhah Khimar Peach Beige',
+        price: 100000,
+        size: 'M'
+    },
+    {
+        img: img7,
+        id: 7,
+        alt: 'Kahla Hijab',
+        name: 'Kahla Hijab',
+        desc: 'Kahla Hijab',
+        price: 79000,
+        size: 'M'
+    },
+    {
+        img: img8,
+        id: 8,
+        alt: 'Shayla Tie-Dye Dress',
+        name: 'Shayla Tie-Dye Dress',
+        desc: 'Shayla Tie-Dye Dress',
+        price: 160000,
+        size: 'All size'
+    },
+    {
+        img: img9,
+        id: 9,
+        alt: 'Zara Abaya Black Series',
+        name: 'Zara Abaya Black Series',
+        desc: 'Zara Abaya Black Series',
+        price: 229000,
+        size: 'All size'
+    },
+    {
+        img: img10,
+        id: 10,
+        alt: 'Zara Abaya Color Series',
+        name: 'Zara Abaya Color Series',
+        desc: 'Zara Abaya Color Series',
+        price: 229000,
+        size: 'All size'
+    },
+]
